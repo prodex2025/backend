425c15c4d34c68ded83ae76a971f0f9ece53adfe
akito <167883398+akito-arakawa@users.noreply.github.com>
2025-07-01 14:40:45 +0900

    リフレッシュトークを利用してアクセストークンを生成する機能を追加

--- backend/src/main/java/com/backend/backend/domain/repository/RefreshTokenRepository.java
+++ backend/src/main/java/com/backend/backend/domain/repository/RefreshTokenRepository.java
@@ -3,7 +3,9 @@ package com.backend.backend.domain.repository;
 import com.backend.backend.domain.model.RefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, UUID> {
+    Optional<RefreshToken> findByToken(String token);
 }
