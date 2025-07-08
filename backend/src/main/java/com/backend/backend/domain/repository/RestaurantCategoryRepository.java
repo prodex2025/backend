fc078043bf09094aaa4f45db59ad9b84b8258e00
akito <167883398+akito-arakawa@users.noreply.github.com>
2025-07-08 22:27:19 +0900

    カテゴリ店舗一覧取得&カテゴリ検索一覧取得メソッドを記述 #23

---
+++ backend/src/main/java/com/backend/backend/domain/repository/RestaurantCategoryRepository.java
@@ -0,0 +1,36 @@
+package com.backend.backend.domain.repository;
+
+import com.backend.backend.domain.model.Restaurant;
+import com.backend.backend.domain.model.RestaurantCategory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface RestaurantCategoryRepository extends JpaRepository<RestaurantCategory, UUID> {
+    //カテゴリごとに表示
+    @Query("""
+            SELECT DISTINCT rc.restaurant FROM RestaurantCategory rc
+            WHERE rc.category.id IN :categoryIds
+            ORDER BY rc.restaurant.createdAt DESC
+            """)
+    Page<Restaurant>findRestaurantsByCategoryIds(@Param("categoryIds") List<UUID> categoryIds, Pageable pageable);
+
+    //カテゴリ選択&検索
+    @Query("""
+    SELECT DISTINCT rc.restaurant FROM RestaurantCategory rc
+    WHERE rc.category.id IN :categoryIds
+    AND rc.restaurant.name LIKE :keyword
+    ORDER BY rc.restaurant.createdAt DESC
+    """)
+    Page<Restaurant> findRestaurantsByCategoryIdsAndKeyword(
+            @Param("categoryIds") List<UUID> categoryIds,
+            @Param("keyword") String keyword,
+            Pageable pageable
+    );
+
+}
