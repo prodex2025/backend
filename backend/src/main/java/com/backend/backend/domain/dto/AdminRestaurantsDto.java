6e0d703da8efa9d32565d924865641a0082ebe3d
akito <167883398+akito-arakawa@users.noreply.github.com>
2025-07-14 00:31:07 +0900

    dtoを作成 #39

---
+++ backend/src/main/java/com/backend/backend/domain/dto/AdminRestaurantsDto.java
@@ -0,0 +1,21 @@
+package com.backend.backend.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AdminRestaurantsDto {
+    private UUID id;
+    private String name;
+    private Boolean isPublished;
+    private Date applicationDate;
+    private Date approval_date;
+    private List<RestaurantCategoryDto> categoryDtoList;
+}
