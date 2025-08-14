c10cd10987de8c37a2ee2b88e96068bdf0186b80
Okishima <kd1348939@st.kobedenshi.ac.jp>
2025-08-14 23:01:21 +0900

    店舗のメニュー取得機能を追加

---
+++ backend/src/main/java/com/backend/backend/domain/service/UserDishService.java
@@ -0,0 +1,46 @@
+package com.backend.backend.domain.service;
+
+import com.backend.backend.domain.dto.DishesListDto;
+import com.backend.backend.domain.model.Dish;
+import com.backend.backend.domain.model.Restaurant;
+import com.backend.backend.domain.repository.DishRepository;
+import com.backend.backend.domain.repository.RestaurantRepository;
+import com.backend.backend.domain.service.mapper.DishMapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.UUID;
+
+@Service
+public class UserDishService {
+    private final DishRepository dishRepository;
+    private final RestaurantRepository restaurantRepository;
+
+    public UserDishService(DishRepository dishRepository,
+                           RestaurantRepository restaurantRepository) {
+        this.dishRepository = dishRepository;
+        this.restaurantRepository = restaurantRepository;
+    }
+
+    private static final int DEFAULT_PAGE_SIZE = 10;
+
+    //店舗のメニュー取得
+    public Page<DishesListDto> getDishes(UUID restaurantId, int page) {
+
+        // 店舗取得（存在チェック）
+        Restaurant restaurant = restaurantRepository.findById(restaurantId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "店舗が存在しません"));
+
+        // ページングしてDishを取得
+        Pageable pageable = PageRequest.of(page, DEFAULT_PAGE_SIZE, Sort.by(Sort.Direction.DESC, "createdAt"));
+        Page<Dish> dishes = dishRepository.findByRestaurantId(pageable, restaurantId);
+
+        return DishMapper.toDishesListDtoPage(dishes);
+    }
+}
