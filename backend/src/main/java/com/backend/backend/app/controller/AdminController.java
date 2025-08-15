2e191c45d0d44f1050ce63778898f967d0c28009
akito <167883398+akito-arakawa@users.noreply.github.com>
2025-08-15 23:24:37 +0900

    公開・非公開の更新処理 #65

--- backend/src/main/java/com/backend/backend/app/controller/AdminController.java
+++ backend/src/main/java/com/backend/backend/app/controller/AdminController.java
@@ -1,15 +1,15 @@
 package com.backend.backend.app.controller;
 
 import com.backend.backend.app.status.StatusFilter;
+import com.backend.backend.domain.dto.AdminRequestIsPublishedDto;
 import com.backend.backend.domain.dto.AdminRestaurantDto;
+import com.backend.backend.domain.model.Restaurant;
 import com.backend.backend.domain.service.AdminRestaurantService;
 import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
@@ -31,6 +31,15 @@ public class AdminController {
                                                        @RequestParam(required = false) List<UUID> categoryIds,
                                                        @RequestParam(required = false, defaultValue = "APPROVED") StatusFilter status) {
         return adminRestaurantService.getRestaurants(page, keyword, categoryIds, status);
+    }
+
+    @PutMapping("/restaurants/{restaurantId}/isPublished")
+    public ResponseEntity<String> editIsPublished(@AuthenticationPrincipal UserDetails userDetails,
+                                                  @PathVariable UUID restaurantId,
+                                                  @RequestBody AdminRequestIsPublishedDto dto) {
+        adminRestaurantService.editIsPublished(userDetails, restaurantId, dto);
+
+        return ResponseEntity.ok("編集完了");
     }
 
 }
--- backend/src/main/java/com/backend/backend/domain/service/AdminRestaurantService.java
+++ backend/src/main/java/com/backend/backend/domain/service/AdminRestaurantService.java
@@ -1,16 +1,23 @@
 package com.backend.backend.domain.service;
 
 import com.backend.backend.app.status.StatusFilter;
+import com.backend.backend.domain.dto.AdminRequestIsPublishedDto;
 import com.backend.backend.domain.dto.AdminRestaurantDto;
 import com.backend.backend.domain.model.Restaurant;
+import com.backend.backend.domain.model.Role;
+import com.backend.backend.domain.model.User;
 import com.backend.backend.domain.repository.RestaurantRepository;
+import com.backend.backend.domain.repository.UserRepository;
 import com.backend.backend.domain.service.specification.RestaurantSpecs;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.domain.Specification;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.UUID;
@@ -19,9 +26,12 @@ import java.util.UUID;
 public class AdminRestaurantService {
 
     private final RestaurantRepository restaurantRepository;
+    private final UserRepository userRepository;
 
-    public AdminRestaurantService(RestaurantRepository restaurantRepository) {
+    public AdminRestaurantService(RestaurantRepository restaurantRepository,
+                                  UserRepository userRepository) {
         this.restaurantRepository = restaurantRepository;
+        this.userRepository = userRepository;
     }
 
     public Page<AdminRestaurantDto> getRestaurants(
@@ -43,4 +53,24 @@ public class AdminRestaurantService {
         // 取得したデータをDTOにセットして値を返す
         return pageData.map(AdminRestaurantDto::from);
     }
+
+    // 公開・非公開の更新
+    @Transactional
+    public void editIsPublished(UserDetails userDetails, UUID restaurantId, AdminRequestIsPublishedDto dto) {
+        // loginId取得
+        String loginId = userDetails.getUsername();
+        // 店舗Idから店舗取得
+        Restaurant restaurant = restaurantRepository.findById(restaurantId)
+                .orElseThrow(()->new RuntimeException("店舗を取得できませんでした"));
+
+        User user = userRepository.findByLoginId(loginId).orElseThrow(()-> new RuntimeException("Userを取得できませんでした"));
+        // 管理者以外がアクセスした場合
+        if (loginId == null || !user.getRole().equals(Role.ROLE_ADMIN)) {
+            throw new AccessDeniedException("権限がありません");
+        }
+        // 更新する値をセット
+        restaurant.setIsPublished(dto.getIsPublished());
+        restaurantRepository.save(restaurant);
+    }
+
 }
